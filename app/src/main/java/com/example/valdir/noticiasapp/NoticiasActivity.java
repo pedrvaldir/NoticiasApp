4acc313d0f1595e39984d31144a04666acca8463
Valdir.Pedroso <pedrvaldir@gmail.com>
2018-02-02 09:13:16 -0200

    AsyncTaskLoader Finalizado.

--- app/src/main/java/com/example/valdir/noticiasapp/NoticiasActivity.java
+++ app/src/main/java/com/example/valdir/noticiasapp/NoticiasActivity.java
@@ -2,7 +2,9 @@ package com.example.valdir.noticiasapp;
 
 import android.content.Intent;
 import android.net.Uri;
-import android.os.AsyncTask;
+import android.app.LoaderManager;
+import android.app.LoaderManager.LoaderCallbacks;
+import android.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,76 +12,41 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class NoticiasActivity extends AppCompatActivity {
+public class NoticiasActivity extends AppCompatActivity implements LoaderCallbacks<List<Noticia>>{
 
     public static final String LOG_TAG = NoticiasActivity.class.getName();
-    private String url = "https://content.guardianapis.com/search?q=curitiba&api-key=test";
+
+    /**
+     * Valor constante para o ID do loader de earthquake. Podemos escolher qualquer inteiro.
+     * Isto só importa realmente se você estiver usando múltiplos loaders.
+     */
+    private static final int NOTICIAS_LOADER_ID = 1;
+
+    private String REQUISICAO_URL_APIGUARDIAN = "https://content.guardianapis.com/search?q=curitiba&api-key=test";
+
+    private NoticiasAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_noticias);
 
-        // Cria uma lista falsa de noticias
-        ArrayList<Noticia> noticias = Uteis.ExtrairNoticias(url);
-
-               NoticiaAsyncTask task = new NoticiaAsyncTask();
-               task.execute(url);
-    }
-
-
-    private class NoticiaAsyncTask extends AsyncTask<String,Void, ArrayList<Noticia>>{
-
-        /**
-         * Esse método é chamado por uma thread em segundo plano, então, podemos executar
-         * operações mais lentas como conexões de rede
-         * <p>
-         * Não é correto atualizar a UI usando uma operação em segundo plano, então, apenas
-         * retornamos {@link ArrayList<Noticia>} como resultado.
-         */
-        @Override
-        protected ArrayList<Noticia> doInBackground(String... urls) {
-
-            // Não execute o pedido se não houver URLs ou se for nula
-            if (urls.length < 1 || urls[0] == null) {
-                return null;
-            }
-
-            // Execute a solicitação HTTP para dados de noticias e processe a resposta.
-            ArrayList<Noticia> resultado = Uteis.buscarNoticias(urls[0]);
-
-            return resultado;
-        }
-
-        /**
-         * Esse método é chamado na UI principal depois que o trabalho em segundo plano foi executado.
-         * <p>
-         * Está correto modificar a UI com esse método. Pegamos o objeto {@link ArrayList<Noticia>} (que
-         * foi retornado do doInBackground()) e atualizamos a tela.
-         */
-        @Override
-        protected void onPostExecute(ArrayList<Noticia> resultado) {
-
-            // Se não houver resultados, não faça nada.
-            if (resultado == null) {
-                return;
-            }
-
-            updateUi(resultado);
-        }
-    }
-
-    private void updateUi(ArrayList<Noticia> ListaNoticia) {
-
-        final NoticiasAdapter noticiasAdapter = new NoticiasAdapter(this, ListaNoticia);
+        // Crie um novo adaptador que leve uma lista vazia de noticias como entrada
+        mAdapter = new NoticiasAdapter(NoticiasActivity.this, new ArrayList<Noticia>());
 
         // Encontra uma referencia de {@link ListView} no layout
         ListView noticiasListView = (ListView) findViewById(R.id.lista);
 
+        mAdapter = new NoticiasAdapter(NoticiasActivity.this, new ArrayList<Noticia>());
+
         // Defina o adaptador no {@link ListView}
         // então a lista pode ser preenchida na interface do usuário
-        noticiasListView.setAdapter(noticiasAdapter);
+        noticiasListView.setAdapter(mAdapter);
+
+        final LoaderManager loaderManager = getLoaderManager();
+        loaderManager.initLoader(NOTICIAS_LOADER_ID, null, NoticiasActivity.this);
 
         //quando alguma noticia for clicada acionar um intent para abrir a url da noticia
         noticiasListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -87,7 +54,7 @@ public class NoticiasActivity extends AppCompatActivity {
                                                     public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
 
                                                         // Achar a noticia que foi clicada
-                                                        Noticia noticiaAtual = noticiasAdapter.getItem(position);
+                                                        Noticia noticiaAtual = mAdapter.getItem(position);
 
                                                         // Converte o URL String em um objeto URI (para passar no construtor de Intent)
                                                         Uri noticiaUri = Uri.parse(noticiaAtual.getUrl());
@@ -98,9 +65,34 @@ public class NoticiasActivity extends AppCompatActivity {
                                                         // Envia o intent para lançar uma nova activity
                                                         startActivity(websiteIntent);
 
-                                                        }
+                                                    }
                                                 }
         );
     }
 
+    @Override
+    public Loader<List<Noticia>> onCreateLoader(int i, Bundle bundle) {
+        // Criar um novo loader para a dada URL
+
+        return new NoticiasLoader(this, REQUISICAO_URL_APIGUARDIAN);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<Noticia>> loader, List<Noticia> noticias) {
+
+        // Limpa o adapter de dados de livros anteriores
+        mAdapter.clear();
+
+        // Se há uma lista válida de {@link livro}s, então os adiciona ao data set do adapter.
+        // Isto ativará a atualização da ListView.
+        if (noticias != null && !noticias.isEmpty()) {
+            mAdapter.addAll(noticias);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<Noticia>> loader) {
+        // Loader reset, so we can clear out our existing data.
+        mAdapter.clear();
+    }
 }
